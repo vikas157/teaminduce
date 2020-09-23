eb4e7e2abe51ff10eaef3ef7967b016290df9060
Vikas cherala <69589147+vikas157@users.noreply.github.com>
2020-09-23 22:51:18 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,7 +182,7 @@
   }
 
   // Navigation active state on scroll
-  var nav_sections = $('section');
+  /*var nav_sections = $('section');
   var main_nav = $('.nav-menu, .mobile-nav');
 
   $(window).on('scroll', function() {
@@ -192,17 +192,17 @@
       var top = $(this).offset().top,
         bottom = top + $(this).outerHeight();
 
-      if (cur_pos >= top && cur_pos <= bottom) {
-        if (cur_pos <= bottom) {
-          main_nav.find('li').removeClass('active');
+        if (cur_pos >= top && cur_pos <= bottom) {
+          if (cur_pos <= bottom) {
+            main_nav.find('li').removeClass('active');
+          }
+          main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
         }
-        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-      }
-      if (cur_pos < 300) {
-        $(".nav-menu ul:first li:first").addClass('active');
-      }
+        if (cur_pos < 300) {
+          $(".nav-menu ul:first li:first").addClass('active');
+        }
     });
-  });
+  });*/
 
   // Intro carousel
   var introCarousel = $(".carousel");
