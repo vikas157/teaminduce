4c8a4e980d8441e12ce6ee1502777bf94cdd6ea4
Vikas cherala <69589147+vikas157@users.noreply.github.com>
2020-10-09 12:18:49 +0530

    1

---
+++ login.js
@@ -0,0 +1,178 @@
+function getInputValue(){
+    var inputVal = document.getElementById("myInput").value;
+    var inputVal1 = document.getElementById("myInput1").value;
+    if (inputVal1=="TSITE12" && inputVal=="vikas35") {
+       // document.getElementById("vikas").click();
+       //location.href="https://drive.google.com/file/d/1ZX1ekyiV0X3uLSXojv5-tRnDMgEJN0sz/view";
+      location.href="https://drive.google.com/drive/u/0/folders/1-Bt2EkL3ivTlFJfTu3UjI6717G2H0ni4";
+    }
+   else if(inputVal1=="TSITE01" && inputVal=="saispandana45"){
+    //document.getElementById("saispandana").click();
+    //location.href="https://drive.google.com/file/d/1x-q9lTsOzjAHaYAdTU2UHGTLrgj_tOnc/view";
+    location.href="https://drive.google.com/drive/u/0/folders/1-DbOcJeeTQ6_IGmNK0Q4YZ1XYkCzNUsO";
+    }
+    else if(inputVal1=="TSITE02" && inputVal=="rajini04"){
+    // document.getElementById("rajini").click();
+    //location.href="https://drive.google.com/file/d/1p9JClF1d4ffQvH4OEukTj1qeB8C8luHW/view"  ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-Ac6lBcanzyDzDzdwaiz4EyAwmffUdfW";
+    }
+    else if(inputVal1=="TSITE03" && inputVal=="sri gouri24"){
+       // document.getElementById("sri gouri").click();
+    //location.href="https://drive.google.com/file/d/1x5wamXnveb2M3jJ5hSfcdTZ9bt9pkpup/view";
+    location.href="https://drive.google.com/drive/u/0/folders/1-KMrqo4YalTEEgMsCtryxqAbdfSHr1BP";
+    }
+   else if(inputVal1=="TSITE04" && inputVal=="sampreeth06"){
+    //document.getElementById("sampreeth").click();
+    //location.href="https://drive.google.com/file/d/19UtBgFPZVkV-TFg9GRq2mFikP1rJyr7K/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-GOGCKmimvYJp3sFMB06Te_hedZEs2yK";
+    }
+   else if(inputVal1=="TSITE05" && inputVal=="preethika48"){
+    //document.getElementById("preethika").click();
+    //location.href="https://drive.google.com/file/d/1ujKGKg32tuWVFeaw36oCZbdhBF64wc0Q/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-QNaCbqla7kjMQoOm_xnkciYjSr7lDt1";
+    }
+    else if(inputVal1=="TSITE06" && inputVal=="pavan10"){
+    //document.getElementById("pavan").click();
+    //location.href="https://drive.google.com/file/d/1htpmIPGDrC5ZbyeJK3LTy3AHdsy_KSTD/view"  ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-ReNlxYg-7J9ciOMrWB0Eg1ACf_z2A5G";
+    }
+    else if(inputVal1=="TSITE07" && inputVal=="aishwarya06"){
+    //document.getElementById("aishwarya").click();
+    //location.href="https://drive.google.com/file/d/1iM5vKxq8y-NkzmPq8mCjoM6FkistF2-2/view"  ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-WRBOeWR-I-GiIe_COuF4rwras9WsX0V";
+    }
+    else if(inputVal1=="TSITE08" && inputVal=="rohini14"){
+    //document.getElementById("rohini").click();
+    //location.href="https://drive.google.com/file/d/196U0QsxTzNR6e4yL3qLSBb9lhiqjKBFC/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-aRL30JE3pa6G2bLcbT7vZemrv5clKOL";
+    }
+    else if(inputVal1=="TSITE09" && inputVal=="aditya46"){
+    //document.getElementById("aditya").click();
+    //location.href="https://drive.google.com/file/d/1WtAWuURSQS8aeIqS96vThuQ1DcyJH13O/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-cpoWqDV53mEFJav6-KTcPGPvGmaUULb";
+    }
+    else if(inputVal1=="TSITE10" && inputVal=="gabbar31"){
+    //document.getElementById("gabbar").click();
+    //location.href="https://drive.google.com/file/d/19U6St9rV-UVRxjmicYngV5tRJa_-JS2A/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-gO0mT_sdKusrXzd5i-CMbXcjMko3-rn";
+    }
+    else if(inputVal1=="TSITE11" && inputVal=="mohan59"){
+   // document.getElementById("mohan").click();
+    //location.href="https://drive.google.com/file/d/1vdjkyN9Avvb6sB2tRP02G5jjQnUAw6WG/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-la9qhPLHZtTcVjBMJLe1KoSjHhpEKRP";
+    }
+    else if(inputVal1=="TSITE13" && inputVal=="sharath49"){
+    //document.getElementById("sharath").click();
+    //location.href="https://drive.google.com/file/d/16ioEyX8ab0DwrdcoHVYS8cmWbilbJUOx/view"  ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-ptsy3rshAk3UzvYppBsJDfoShXYaxk5";
+    }
+    else if(inputVal1=="TSITE14" && inputVal=="nagajyothi20"){
+    //document.getElementById("nagajyothi").click();
+    //location.href="https://drive.google.com/file/d/1hBPOpFwmh9IIq4oH1TMtBcQX7Rg20jTT/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-yhtv0TXy5hPW8y4bLPRmAcEZgcdqa1H";
+    }
+    else if(inputVal1=="TSITE15" && inputVal=="sanjana28"){
+    //document.getElementById("sanjana").click();
+    //location.href="https://drive.google.com/file/d/1eSYpprjHw5B1b3ESr2aDmq76xKXB3fo0/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-ztTngmvi-N_WPPnivEncn7JViG9zGhv";
+    }
+    else if(inputVal1=="TSITE16" && inputVal=="abhivardhan54"){
+    //document.getElementById("abhivardhan").click();
+    //location.href="https://drive.google.com/file/d/1xfhPeFBNvgydZRwwkCSqCVSEAtU5_V8Y/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/10-Tf03ghBcXtui2QwIFFwXhYml7wsJ__";
+    }
+    else if(inputVal1=="TSITE17" && inputVal=="srikar02"){
+   // document.getElementById("srikar").click();
+    //location.href="https://drive.google.com/file/d/17-w642YFm2clN99Jh1oUuNOoydRm-R_l/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/1-BwF-YgbGUmfTab_noDQn5MjO_4uNmvT";
+    }
+    else if(inputVal1=="TSITE18" && inputVal=="sairam33"){
+   // document.getElementById("sairam").click();
+    //location.href="https://drive.google.com/file/d/1aFsJTbsfg7UbR5S_IbxqQ-tQEddyqf3a/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/101ldmdq6l0ml5MrCvDnqc4lDP4QlCxuv";
+    }
+    else if(inputVal1=="TSITE19" && inputVal=="susmitha22"){
+   // document.getElementById("susmitha").click();
+    //location.href="https://drive.google.com/file/d/1p9vvdhCK0mVQdG6UndXHNE-ElU7G6K7T/view" ;
+    location.href="https://drive.google.com/drive/u/0/folders/103U7BpNJyTaKEfb5ke4WpOVEL68rZDJf";
+    }
+    else if(inputVal1=="TSITE20" && inputVal=="smaran54"){
+   // document.getElementById("smaran").click();
+   //location.href="https://drive.google.com/file/d/1EiJW0wTIBfE3AANl7BtMFtAgd9fhubv3/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/10QMv3NzL-K6om3Xf9VJT2Ad9nMfRTvDj";
+    }
+    else if(inputVal1=="TSITE21" && inputVal=="shirisha58"){
+   // document.getElementById("shirisha").click();
+   //location.href="https://drive.google.com/file/d/1lZrG0wuoNKTpn_eENhqfLLfbD_k859iV/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/10Z0jnPowppodB8Da-Jfny7U7LqZAZYp0";
+    }
+    else if(inputVal1=="TSITE22" && inputVal=="supragna09"){
+   // document.getElementById("supragna").click();
+   //location.href="https://drive.google.com/file/d/1H__VbbJ8PugHrCbaZHI5hh8NjsKrNbWP/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/10nerOAbw7esL0cXO3uUw4GtVclfykG2Z";
+    }
+    else if(inputVal1=="TSITE23" && inputVal=="raja prithvi10"){
+   // document.getElementById("raja prithvi").click();
+   //location.href="https://drive.google.com/file/d/1jYihe3eF0EnN7b4yxYlNO7jjVW1zbxow/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/10vdCxUO4UWpziDPL_r7cPm6ZTSKSOsbD";
+    }
+    else if(inputVal1=="TSITE24" && inputVal=="shirisha54"){
+   // document.getElementById("shirisha").click();
+   location.href="https://drive.google.com/drive/u/0/folders/10cGGtzwGkWE1wKfHh0qs-UP-uq17727s" ;
+    }
+    else if(inputVal1=="TSITE25" && inputVal=="kamal58"){
+   // document.getElementById("kamal").click();
+   //location.href="https://drive.google.com/file/d/1uJBWPDvNa5KrhVscSsXNzUHr5Pfhn0hD/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/10vejPzBfLf_9LzIGN72FCFX3R3sqov8W";
+    }
+    else if(inputVal1=="TSITE26" && inputVal=="amulya36"){
+   // document.getElementById("amulya").click();
+   //location.href="https://drive.google.com/file/d/1TtnenZwcCjM0b0Lsa9O05vgUWSuu_OKT/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/1-DOtzjNUFNmcM-5VA1v9kuBeKHAi-8ij";
+    }
+    else if(inputVal1=="TSITE27" && inputVal=="uthej16"){
+   // document.getElementById("uthej").click();
+   //location.href="https://drive.google.com/file/d/1NWUTy-iMaap8RGrN1nfrIiXeq-7dFb30/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/10vfjvCrevFq9fOGim2pHbJ_Fqje7aOgx";
+    }
+    else if(inputVal1=="TSITE28" && inputVal=="sagar15"){
+   // document.getElementById("sagar").click();
+   //location.href="https://drive.google.com/file/d/11Iv30e3OP2slbrQQ7A2Gyxx0nfyBZ_Qj/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/10zL53nflkYSvJhLHriQlrUDYzGbZSaio";
+    }
+    else if(inputVal1=="TSITE29" && inputVal=="vinitha39"){
+   // document.getElementById("vinitha").click();
+   //location.href="https://drive.google.com/file/d/1PAS5VhAol_96pdWP_PuhtJnuuYkdv1dk/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/10zY28JihV_1y7PFWtgvLlY3yy-NA9jkY";
+    }
+    else if(inputVal1=="TSITE30" && inputVal=="aitha06"){
+   // document.getElementById("aitha").click();
+   //location.href="https://drive.google.com/file/d/1CDFIZ23ewg0vqc6ms14U8D1AgZiMLPN4/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/10zwsiM3xBt1mBJ830HR64rN1NB3pOlY5";
+    }
+    else if(inputVal1=="TSITE31" && inputVal=="deepika16"){
+   // document.getElementById("deepika").click();
+   //location.href="https://drive.google.com/file/d/18MbLnIjQnMuaqsd6ptT7pccqciCIDY5g/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/110mFY2UB1XUSjC29K1KP2pMfJqksxvLc";
+    }
+    else if(inputVal1=="TSITE32" && inputVal=="akshith03"){
+   // document.getElementById("akshith").click();
+   //location.href="https://drive.google.com/file/d/1b_MDCfS1mUdt2vmciCMFhYA5RSwY5DvA/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/112HRmMrjw_IfkrFzeUY_X5UdekUBSBUx";
+    }
+    else if(inputVal1=="TSITE33" && inputVal=="vanitha52"){
+   // document.getElementById("vanitha").click();
+   //location.href="https://drive.google.com/file/d/1CDFIZ23ewg0vqc6ms14U8D1AgZiMLPN4/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/116OuaADumLuq8xmVbyLCmReNbQ0Urg7_";
+    }
+    else if(inputVal1=="TSITE34" && inputVal=="vaibhav00"){
+   // document.getElementById("vaibhav").click();
+   //location.href="https://drive.google.com/file/d/1P5CxCXwz1C2_Xp9ZJ8WORVRR57YBg1YE/view" ;
+   location.href="https://drive.google.com/drive/u/0/folders/11FRT6wyT8HSPmq-r9d7AbRGWAyyEnQtd";
+    }
+    else{
+        alert("wrong User ID or password");
+    }
+       
+    
+}
